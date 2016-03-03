8969daf62846617a991e9968a119e17091c8b6f0
Nathan Ross Powell <nathanrosspowell@gmail.com>
2016-03-03 17:48:34 -0500

    Working Pro Micro numpad firmware

---
+++ flash.ps1
@@ -0,0 +1,5 @@
+& 'C:\Program Files (x86)\Arduino\hardware\tools\avr\bin\avrdude.exe' -C 'C:\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf' -vvvv -p atmega32u4 -c avr109 -P com4 -b57600 -D -V -U flash:w:"C:\Users\Nathan\Documents\GitHub\tmk\keyboard\onekey\onekey_lufa.hex":i
+
+
+
+
